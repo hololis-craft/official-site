7a6a19295160c40b41e4cb483aab20f1d0d57cb3
f0reachARR <f0reach@f0reach.me>
2026-07-31 16:33:37 +0900

    change url

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from "astro/config";
 import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
-  site: "https://hololiscraft.f0reach.me",
-  adapter: cloudflare()
+  site: "https://hololis.f0reach.me",
+  adapter: cloudflare(),
 });
